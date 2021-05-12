832766a428fab0df429b992557324ea2d09c6683
UnivDev <univdev99@gmail.com>
2021-05-12 08:11:51 -0700

    add whitelist

--- contracts/Mint.sol
+++ contracts/Mint.sol
@@ -12,9 +12,24 @@ contract Mint is ERC1155, Ownable {
     // collection counter
     uint collection_counter;
 
+
+    // nft : bytes32 (hash_), address (owner_), uint (price_)
+    struct NFT {
+        bytes32 hash_;
+        address payable owner_;
+        uint price_;
+    }
+    // nft list uint(id) => NFT(nft)
+    mapping (uint => NFT) nft_list;
+
+
+
     // NFT card list uint (id) => string (hash) 
     // this list is according to cards, not collection
     mapping (uint => bytes32) nft_hash_list;
+    // nft list price 
+    // uint (nft_global_id) => uint(price)
+    mapping (uint => uint) nft_price;
     // NFT card list every collection
     // uint (collection_id) => uint (nft_collection_id) => uint (nft_global_id)
     mapping (uint => mapping(uint => uint)) nft_per_collection;
@@ -58,9 +73,16 @@ contract Mint is ERC1155, Ownable {
 
     // mint single NFT 
     // params: NFT hash, collection ID, amount
-    function single_mint (bytes32 _hash, uint _collection_id, uint _amount) public returns (uint) {
-        // store hash value of the new nft
-        nft_hash_list[nft_counter] = _hash;
+    function single_mint (bytes32 _hash, uint _collection_id, uint _amount, uint _price) public returns (uint) {
+
+
+        nft_list[nft_counter].hash_ = _hash;
+        nft_list[nft_counter].owner_ = payable(msg.sender);
+        nft_list[nft_counter].price_ = _price;
+
+        // // store hash value of the new nft
+        // nft_hash_list[nft_counter] = _hash;
+        // nft_price[nft_counter] = _price;
 
         uint collection_capacity = nft_counter_per_collection[_collection_id];
         // store the nft global id with collection id and nft_collection_id
@@ -80,14 +102,15 @@ contract Mint is ERC1155, Ownable {
     //     bytes32[2] memory others = [bytes32("one"), bytes32("two")];
     //     return len;
     // }
-    function batch_mint (bytes32[] memory _hashes, uint[] memory _collection_ids, uint[] memory _amounts) public returns (uint) {
+    function batch_mint (bytes32[] memory _hashes, uint[] memory _collection_ids, uint[] memory _amounts, uint[] memory _prices) public{
         // require : length of _hashes and _collection_ids and _amounts have to equal
         require(_hashes.length == _collection_ids.length, "`_hashes` size has to be equal to `_collection_ids` size");
         // the size of nfts which has to be minted
         uint length = _hashes.length;
         for (uint i = 0 ; i < length ; i++) {
-            single_mint(_hashes[i], _collection_ids[i], _amounts[i]);
+            single_mint(_hashes[i], _collection_ids[i], _amounts[i], _prices[i]);
         }
+
     }
 
     // Get nft hash by nft id
@@ -95,7 +118,8 @@ contract Mint is ERC1155, Ownable {
         // require : _nft_id is available, so can not be exceed to total nft counter
         require(_nft_id < nft_counter, "nft id can not be exceed to the total nft counter");
 
-        return nft_hash_list[_nft_id];
+        // return nft_hash_list[_nft_id];
+        return nft_list[_nft_id].hash_;
     }
 
     // Get total nft counter
@@ -120,10 +144,55 @@ contract Mint is ERC1155, Ownable {
 
         selling_list[selling_list_counter] = nft_global_id;
         selling_list_counter++;
+
+        // To do list : calculate listing fee and transfer to wallet
         return selling_list_counter;
     }
     // remove nft from selling list
-    function remove_selling_item (uint nft_global_id) public returns(uint) {
+    function remove_selling_item (uint nft_global_id) public onlyOwner returns (bool) {
+        // require : nft_global_id has to be available under the nft global counter
+        require(nft_global_id < nft_counter, "nft id has to be under the nft global counter");
+
+        // remove nft from nft selling list
+        for (uint i = 0 ; i < selling_list_counter ; i++) {
+            if(selling_list[i] == nft_global_id){
+                delete selling_list[i];
+                return true;
+            }
+        }
+        return false;
+    }
+    // calculate listing fee
+    // 1% fee
+    function listing_fee (uint nft_global_id) public view returns(uint) {
 
     }
+
+    // check if nft is in the selling list by nft_id
+    function is_selling_list (uint nft_global_id) public view returns (bool) {
+        // require : nft_global_id has to be available under the nft global counter
+        require(nft_global_id < nft_counter, "nft id has to be under the nft global counter");
+
+        // check if nft from nft selling list
+        for (uint i = 0 ; i < selling_list_counter ; i++) {
+            if(selling_list[i] == nft_global_id){
+                return true;
+            }
+        }
+        return false;
+    }
+    // buying nft 
+    function buy_nft (uint _nft_id, uint amount_) public payable {
+        // require : nft_global_id has to be less than totla nft counter
+        require(_nft_id < nft_counter, "nft_global_id has to be less than totla nft counter");
+        // require : check if the nft is on the selling list
+        require(is_selling_list(_nft_id), "nft has to be on the selling list");
+
+        // Current owner of nft_id
+        address payable current_owner = nft_list[_nft_id].owner_ ;
+        // the new owner of nft_id
+        address payable new_owner = payable(msg.sender);
+        
+    }
+
 }
---
+++ contracts/Whitelist.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Whitelist is Ownable{
+    event MemberAdded (address member);
+    event MemberRemoved (address member);
+
+    mapping (address => bool) members;
+
+    /**
+    * @dev The contract constructor
+     */
+    constructor() public Ownable() {}
+
+    function isMember(address _member) public view returns(bool) {
+        return members[_member];
+    }
+
+    function addMember (address _member) public onlyOwner {
+        require(!isMember(_member), "Address is member already.");
+        members[_member] = true;
+        emit MemberAdded(_member);
+    }
+
+    function removeMember(address _member) public onlyOwner {
+        require(isMember(_member), "Not member of whitelist");
+        delete members[_member];
+        emit MemberRemoved(_member);
+    }
+}
